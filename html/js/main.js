7056e14ab87a323cf376da1617fbf454062e4242
Sven Stucki <git@svenstucki.ch>
2016-07-27 18:44:44 +0200

    Combine React JS files

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,15 @@
+var FileView = React.createClass({
+
+  render: function () {
+    return (
+      <div className="fileView">
+        <h2>FileView</h2>
+
+      </div>
+    );
+  },
+
+});
+
+
 ReactDOM.render(<FileView file="test" />, document.getElementById('content'));
