c15b98129e155a3265b1c7d576d2f1e3953ba650
Sven Stucki <git@svenstucki.ch>
2016-07-27 14:50:45 +0200

    Switch to fsnotify, implement basic routing (manually, switch to gorilla/mux in near future)

--- main.go
+++ main.go
@@ -1,30 +1,79 @@
 package main
 
 import (
-	"golang.org/x/exp/inotify"
+	"github.com/fsnotify/fsnotify"
+	"html"
 	"log"
+	"net/http"
+	"os"
 )
 
 const FN = "test"
 
 func main() {
-	watcher, err := inotify.NewWatcher()
+	// watcher setup
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Panic(err)
 	}
+	defer watcher.Close()
 
-	err = watcher.Watch(FN)
-	if err != nil {
-		log.Panic(err)
-	}
+	// wait for file changes in background
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case evt := <-watcher.Events:
+				log.Println("Event: " + evt.String())
+				log.Printf("Evt in file '%s', command: 0x%x\n", evt.Name, evt.Op)
 
-	for {
-		select {
-		case evt := <-watcher.Event:
-			log.Println("Event: " + evt.String())
-		case err := <-watcher.Error:
-			log.Print("Error: ")
-			log.Println(err)
+			case err := <-watcher.Errors:
+				log.Print("Error: ")
+				log.Println(err)
+			}
 		}
+	}()
+
+	// add files to watch
+	err = watcher.Add(FN)
+	if err != nil {
+		log.Panic(err)
 	}
+
+	// setup and start server
+	server()
+
+	<-done
+}
+
+func server() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got http request for: " + html.EscapeString(r.URL.Path))
+
+		path := r.URL.Path[1:]
+		if path == "" {
+			path = "index.html"
+		}
+
+		// serve file with this name if it exists
+		fn := "./html/" + path
+		if _, err := os.Stat(fn); err == nil {
+			// serve static content
+			http.ServeFile(w, r, fn)
+			return
+		}
+
+		// return 404 error
+		httpErrorHandler(w, r, http.StatusNotFound)
+	})
+
+	go func() {
+		log.Println("Starting server...")
+		http.ListenAndServe(":8080", nil)
+	}()
+}
+
+func httpErrorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	log.Printf("Error serving '%s': %d", r.URL.Path, status)
 }
